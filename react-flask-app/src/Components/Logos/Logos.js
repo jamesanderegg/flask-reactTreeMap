d2e72c4b22272d8c46edf5d79b82b3e548447011
Juicyjames <iceandjames@gmail.com>
2020-08-13 15:18:45 -0600

    changed the size of the logos

--- react-flask-app/src/Components/Logos/Logos.js
+++ react-flask-app/src/Components/Logos/Logos.js
@@ -1,14 +1,27 @@
-import React from 'react'
-import './logos.css'
+import React from "react";
+import "./logos.css";
 
-const Logos = ()=>{
-return(
+const Logos = () => {
+  return (
     <ul className="logos-ul">
-        <li className="logos-li"><i className="fab fa-python python" alt="Python"></i>Python</li>
-        <li className="logos-li">Javascript<i className="fab fa-js javascript" alt="Javascript"></i></li>
-        <li className="logos-li">D3<i className="fab fa-react react" alt="React"></i>React</li>
-        <li className="logos-li"><img className="logos-image" src="http://localhost:3000/uploads/images/power-bi.png" alt="Microsoft Power Bi"></img>Power Bi</li>
+      <li className="logos-li">
+        <i className="fab fa-python python" alt="Python"></i> Python
+      </li>
+      <li className="logos-li">
+        JavaScript <i className="fab fa-js javascript" alt="Javascript"></i>
+      </li>
+      <li className="logos-li">
+        D3 <i className="fab fa-react react" alt="React"></i>React
+      </li>
+      <li className="logos-li">
+        <img
+          className="logos-image"
+          src="http://localhost:3000/uploads/images/power-bi.png"
+          alt="Microsoft Power Bi"
+        ></img>{" "}
+        Power Bi
+      </li>
     </ul>
-)
-}
+  );
+};
 export default Logos;
